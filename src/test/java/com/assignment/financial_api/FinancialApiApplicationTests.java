5344e95b29ef7e98e34c2380972af28413b8d455
mostefa <mostefa.aliarous-ext@energisme.com>
2025-10-07 11:36:34 +0100

    spring project init

---
+++ src/test/java/com/assignment/financial_api/FinancialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.financial_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
